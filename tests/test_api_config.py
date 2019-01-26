4ad617c5f038d2c3ea8029819d2b0e76251914d2
Mr. Senko <atodorov@mrsenko.com>
2019-01-26 12:01:46 +0200

    flake8

--- tests/test_api_config.py
+++ tests/test_api_config.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class GivenConfigurationFileExists(unittest.TestCase):
     @unittest.skip('should be implemented in tcms-api')
     def test_when_plugin_initializes_then_uses_config(self):

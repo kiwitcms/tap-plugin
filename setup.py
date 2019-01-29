8e089141d0377b7c9dae95ad25b8f9f4103a74d3
Mr. Senko <atodorov@mrsenko.com>
2019-01-30 00:20:21 +0200

    Version 0.1.1 with new badges

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIREMENTS = open('requirements.txt').readlines()
 
 
 setup(name='kiwitcms-tap-plugin',
-      version='0.1.0',
+      version='0.1.1',
       packages=['tcms_tap_plugin'],
       scripts=['tcms-tap-plugin'],
       description='Test Anything Protocol (TAP) plugin for '

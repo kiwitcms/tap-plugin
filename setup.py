a8ce984b0e7c0c75c3d64a439ce2d5344d6911d0
Mr. Senko <atodorov@mrsenko.com>
2020-10-28 22:21:00 +0200

    Changelog for v8.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIREMENTS = open('requirements.txt').readlines()
 
 
 setup(name='kiwitcms-tap-plugin',
-      version='8.3',
+      version='8.4',
       packages=['tcms_tap_plugin'],
       scripts=['tcms-tap-plugin'],
       description='Test Anything Protocol (TAP) plugin for '

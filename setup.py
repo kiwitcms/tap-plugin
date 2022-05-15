ed75f99c07aa44d2eb11be186e7b1188eb6b2504
Alexander Todorov <atodorov@mrsenko.com>
2022-05-15 22:49:37 +0300

    Annotate plugin with name/version attributes

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 # pylint: disable=missing-docstring
+import os
 from setuptools import setup
+
+
+def get_version():
+    version_py_path = os.path.join('tcms_tap_plugin', 'version.py')
+    with open(version_py_path, encoding="utf-8") as version_file:
+        version = version_file.read()
+        return version.replace(
+            ' ', ''
+        ).replace('__version__=', '').strip().strip("'").strip('"')
 
 
 with open('README.rst', encoding="utf-8") as readme:
     LONG_DESCRIPTION = readme.read()
-
 
 with open('requirements.txt', encoding="utf-8") as requirements:
     REQUIREMENTS = requirements.readlines()
 
 
 setup(name='kiwitcms-tap-plugin',
-      version='11.0',
+      version=get_version(),
       packages=['tcms_tap_plugin'],
       scripts=['tcms-tap-plugin'],
       description='Test Anything Protocol (TAP) plugin for '
